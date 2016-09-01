ce686eab8bf2023c6150f63d475a64f72cea8660
Catherine Wei <weizhimiao@icloud.com>
2016-09-01 10:17:07 +0800

    Update controllers.js

--- app/controllers.js
+++ app/controllers.js
@@ -14,16 +14,16 @@ CAControllers.controller('TimetableCtrl', ['$scope',
     function ($scope){
         document.getElementById("NavTitle").innerHTML = "Catherine ♥︎ Ant"
 
-	var d = new Date();
-	//得到1970年一月一日到现在的秒数
-	var len = d.getTime();
-	//本地时间与GMT时间的时间偏移差
-	var offset = d.getTimezoneOffset() * 60000;
-	//得到现在的格林尼治时间
-	var utcTime = len + offset;
-	// 洛杉矶时间
-	var currentDate = new Date(utcTime + 3600000 * -7);
-            
+        var d = new Date();
+        //得到1970年一月一日到现在的秒数
+        var len = d.getTime();
+        //本地时间与GMT时间的时间偏移差
+        var offset = d.getTimezoneOffset() * 60000;
+        //得到现在的格林尼治时间
+        var utcTime = len + offset;
+        // 洛杉矶时间
+        var currentDate = new Date(utcTime + 3600000 * -7);
+
         $scope.today = (new Date(utcTime + 3600000 * -7)).toString('ddd MMM dd yyyy');
 
         $scope.getUpAt = ""
@@ -43,7 +43,7 @@ CAControllers.controller('TimetableCtrl', ['$scope',
         } else if (!(new Date(utcTime + 3600000 * -7)).addDays(2).is().weekday()) {
             $scope.nextHoliday = "2 days😃"
         } else {
-            var dayDif = Math.ceil(((new Date(utcTime + 3600000 * -7)).next().saturday().getTime() - (new Date(utcTime + 3600000 * -7)).getTime()) /  1000  /  60  /  60  / 24) - 1
+            var dayDif = parseInt(((new Date(utcTime + 3600000 * -7)).next().saturday().getTime() - (new Date(utcTime + 3600000 * -7)).getTime()) /  1000  /  60  /  60  / 24) - 1
             $scope.nextHoliday = dayDif + " days😃"
         }
     }
@@ -117,13 +117,13 @@ CAControllers.controller('HomeworkCtrl', ['$scope', '$wilddogArray', "$window",
         $scope.markDone = function () {
             ref.child(selectedHomeworkId).update({"done":"YES", "doneAt":Wilddog.ServerValue.TIMESTAMP});
             // $scope.hideModal();
-            $window.location.href = "#homework";
+            $window.location.href = "#assignments";
         }
 
         $scope.deleteHomework = function () {
             ref.child(selectedHomeworkId).remove();
             // $scope.hideModal();
-            $window.location.href = "#homework";
+            $window.location.href = "#assignments";
         }
 
         $scope.getTimeText = function (dateText) {
@@ -189,6 +189,9 @@ CAControllers.controller('HomeworkCtrl', ['$scope', '$wilddogArray', "$window",
         $scope.todoList = [];
         $scope.doneList = [];
         $scope.lateList = [];
+
+        // 奖励花花
+        $scope.flowerCount = 0;
         
         $scope.todo.$loaded().then(function () {
             //to make sure that $scope.blogs is already loaded, otherwise length doesn't exist
@@ -207,23 +210,32 @@ CAControllers.controller('HomeworkCtrl', ['$scope', '$wilddogArray', "$window",
                 if (status == "late") {
                     $scope.lateList.push($scope.todo[i]);
                 } else if (status == "nohurry") {
-                    $scope.todo[i].nohurry = true
+                    $scope.todo[i].nohurry = true;
                     $scope.todoList.push($scope.todo[i]);
                 } else {
-                    $scope.todo[i].nohurry = false
+                    $scope.todo[i].nohurry = false;
                     $scope.todoList.push($scope.todo[i]);
                 }
+            }
+
+            if ($scope.lateList.length == 0) {
+                $("#no-late-work-notif").show();
+                $scope.flowerCount += 10;
+            } else {
+                $("#no-late-work-notif").hide();
             }
         });
         $scope.done.$loaded().then(function () {
             $scope.doneList = [];
 
             for(var i=0;i<$scope.done.length;i++){
+                $scope.flowerCount += $scope.done[i].type == "Test" ? 10 : ($scope.done[i].type == "Quiz" ? 3 : 1);
+
                 var dateText = $scope.done[i].deadline
                 if (dateText.length > 10) {
-                    dateText = dateText.substring(0,10) + "T" + dateText.substring(11)
+                    dateText = dateText.substring(0,10) + "T" + dateText.substring(11);
                 }
-                $scope.done[i].timestamp = Date.parse(dateText).getTime()
+                $scope.done[i].timestamp = Date.parse(dateText).getTime();
 
                 $scope.doneList.push($scope.todo[i]);
             }
@@ -248,379 +260,18 @@ CAControllers.controller('HomeworkNewCtrl', ['$scope', '$wilddogArray', "$window
                 createAt:Wilddog.ServerValue.TIMESTAMP,
             });
 
-            $window.location.href = "#homework";
+            $window.location.href = "#assignments";
         };
     }
 ]);
 
-CAControllers.controller('HomeworkEditCtrl', ['$scope', '$wilddogObject', "$window", "$routeParams",
-    function ($scope, $wilddogObject, $window, $routeParams){
+CAControllers.controller('HomeworkEditCtrl', ['$scope', '$wilddogObject', "$routeParams",
+    function ($scope, $wilddogObject, $routeParams){
         
         var ref = new Wilddog("https://chengkang.wilddogio.com/homework/"+$routeParams.id);
         // 创建一个同步数组
         var data = $wilddogObject(ref);
 
         data.$bindTo($scope,"homework")
-    }
-]);
-
-
-CAControllers.controller('OptionCtrl', ['$scope', "OptionService", "AuthService",
-    function ($scope, OptionService, AuthService){
-        $scope.option =  OptionService.basic();
-        $scope.AuthService = AuthService;
-
-        $scope.option.$loaded().then(function () {
-            $scope.l = CONFIG_LANG[$scope.option.language==undefined?"en":$scope.option.language];
-        });
-
-
-    }
-]);
-
-CAControllers.controller('BlogListCtrl', ['$scope', "BlogService", "OptionService",
-	function ($scope, BlogService, OptionService){
-        $scope.current_page = 1;
-        $scope.count = 0;
-        $scope.perpage = 10;
-
-        $scope.blogs = BlogService.getAll();
-
-        $scope.blogs.$loaded().then(function () {
-            //to make sure that $scope.blogs is already loaded, otherwise length doesn't exist
-            $scope.count = $scope.blogs.length;
-        });
-
-        $scope.totalPage = function() {
-            //if $scope.count==0, means at this moment the data hasn't been loaded.
-            return $scope.count==0?1:Math.ceil($scope.count/$scope.perpage);
-        }
-
-        //currentN is the number of articles of the current page.
-        $scope.currentN = function() {
-            var n = $scope.count - ($scope.current_page-1)*$scope.perpage;
-            return n>=10?10:n;
-        }
-
-        $scope.hasPrev = function() {
-            return $scope.current_page==1?false:true;
-        }
-
-        $scope.hasNext = function() {
-            return $scope.totalPage()==$scope.current_page?false:true;
-        }
-
-        $scope.prevPage = function() {
-            if(($scope.current_page--)<1){
-                $scope.current_page = 1;
-            }
-        }
-
-        $scope.nextPage = function() {
-            if(($scope.current_page++)>$scope.totalPage()){
-                $scope.current_page = $scope.totalPage();
-            }
-        }
-
-        $scope.range = function(n) {
-            return new Array(n);
-        };
-
-        $scope.BlogService = BlogService;
-
-        var page_name = "HOME";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("home");
-    }
-]);
-
-
-CAControllers.controller('BlogDetailCtrl', ['$scope', "$location", "BlogService", "OptionService", "$sce", "$routeParams",
-	function ($scope, $location, BlogService, OptionService, $sce, $routeParams){
-        $scope.trustAsHtml = $sce.trustAsHtml;
-        $scope.location = $location;
-        $scope.blogs =  BlogService.getAll();
-
-        $scope.toggleDuoshuoComments = function(container,blog){
-            var el = document.createElement('div');//该div不需要设置class="ds-thread"
-            el.setAttribute('data-thread-key', blog.$id);//必选参数
-            el.setAttribute('data-url', $location.absUrl());//必选参数
-            el.setAttribute('data-title', blog.title);//可选参数
-            DUOSHUO.EmbedThread(el);
-            $(container).html("");
-            $(container).append(el);
-        }
-
-        $scope.blogs.$loaded().then(function () {
-            $scope.blog = $scope.blogs.$getRecord($routeParams.blogId);
-            var page_name = $scope.blog.title;
-            var site_name = OptionService.setSiteTitle(page_name);
-            OptionService.setCurrentNav("");
-        });
-    }
-]);
-
-
-CAControllers.controller('BlogPostCtrl', ['$scope', "OptionService", "BlogService", "$window",
-	function ($scope, OptionService, BlogService, $window){
-        $scope.allCats = BlogService.getAllCats();
-        $scope.allTags = BlogService.getAllTags();
-
-		$scope.title = "";
-		$scope.cat = "";
-		$scope.tags = "";
-		$(editor.getElement('editor').body).html("");
-		$($(editor.getElement('previewer').body).children()[0]).html("");
-
-        $scope.selectCat = function(catname) {
-            $scope.cat = catname;
-        }
-
-        $scope.addTag = function(tagname) {
-            $scope.tags += " "+tagname;
-        }
-
-        $scope.clearCat = function() {
-            $scope.cat = $scope.cat.split(' ')[0].toUpperCase();
-        }
-
-        $scope.clearTags = function() {
-            $scope.tags = $.trim($scope.tags.toLowerCase());
-        }
-
-		$scope.newBlog = function() {
-            var raw_content = $(editor.getElement('editor').body).html();
-            editor.preview();
-            var content = $($(editor.getElement('previewer').body).children()[0]).html();
-            if( $scope.title != "" && raw_content != ""){
-                var blogsRef = new Firebase("https://github-pages.firebaseio.com/users/"+CONFIG_UID+"/blogs/");
-                var date = new Date().getTime();
-                var id = date;
-
-                $scope.clearTags();
-                $scope.clearTags();
-                var cat = $scope.cat==""?"uncategorized":$scope.cat;
-                var tags = $scope.tags;
-                blogsRef.child(""+id).set({
-                    title: $scope.title,
-                    content: content,
-                    raw_content: raw_content,
-                    date: date,
-                    snippet: "",
-                    cat: cat,
-                    tags: tags,
-                });
-                $scope.title = "";
-                $(editor.getElement('editor').body).html("")
-				$($(editor.getElement('previewer').body).children()[0]).html("");
-                BlogService.refreshData();
-                $window.location.href = "#/p="+id
-            }
-	    };
-
-        var page_name = "NEW";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("");
-    }
-]);
-
-
-CAControllers.controller('BlogEditCtrl', ['$scope', "OptionService", "BlogService", "$window", "$firebaseObject", "$sce", "$routeParams",
-	function ($scope, OptionService, BlogService, $window, $firebaseObject, $sce, $routeParams){
-        var blogRef = new Firebase("https://github-pages.firebaseio.com/users/"+CONFIG_UID+"/blogs/"+$routeParams.blogId);
-        var blog =  $firebaseObject(blogRef);
-
-        $scope.title = "";
-        $scope.cat = "";
-        $scope.tags = "";
-        $scope.allCats = BlogService.getAllCats();
-        $scope.allTags = BlogService.getAllTags();
-
-        $scope.selectCat = function(catname) {
-            $scope.cat = catname;
-        }
-
-        $scope.addTag = function(tagname) {
-            $scope.tags += " "+tagname;
-        }
-
-        $scope.clearCat = function() {
-            $scope.cat = $scope.cat.split(' ')[0].toUpperCase();
-        }
-
-        $scope.clearTags = function() {
-            $scope.tags = $scope.tags==undefined?"":$.trim($scope.tags.toLowerCase());
-        }
-
-		blog.$loaded()
-		  .then(function(data) {
-	  		$scope.blog = data;
-			$scope.title = $scope.blog.title;
-			$scope.cat = $scope.blog.cat;
-			$scope.tags = $scope.blog.tags;
-	        $(editor.getElement('editor').body).html(data.raw_content);
-
-		  })
-		  .catch(function(error) {
-		    console.error("Error:", error);
-		  });
-
-		$scope.editBlog = function() {
-            var raw_content = $(editor.getElement('editor').body).html();
-            editor.preview();
-            var content = $($(editor.getElement('previewer').body).children()[0]).html();
-            if( $scope.title != "" && raw_content != ""){
-                var date = new Date().getTime();
-
-                $scope.clearCat();
-                $scope.clearTags();
-                var cat = $scope.cat==""?"UNCATEGORIZED":$scope.cat;
-                var tags = $scope.tags;
-                blog.title = $scope.title;
-                blog.content = content;
-                blog.raw_content = raw_content;
-                blog.date = date;
-                blog.cat = cat;
-                blog.tags = tags;
-                blog.$save();
-                $(editor.getElement('editor').body).html("");
-                BlogService.refreshData();
-                $window.location.href = "#/p="+blog.$id;
-            }
-	    };
-
-        var page_name = "EDIT";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("");
-    }
-]);
-
-
-CAControllers.controller('CatAllCtrl', ['$scope', "BlogService", "OptionService",
-	function ($scope, BlogService, OptionService){
-        $scope.cats = BlogService.getAllCats();
-//        $scope.catMax = BlogService.getCatMax();
-        $scope.BlogService = BlogService;
-
-        var page_name = "CATEGORIES";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("cats");
-    }
-]);
-
-CAControllers.controller('CatOneCtrl', ['$scope', "BlogService", "OptionService", "$routeParams",
-	function ($scope, BlogService, OptionService, $routeParams){
-        $scope.cats = BlogService.getAllByCat();
-        $scope.catname = $routeParams.catname;
-
-        $scope.years = {};
-
-        var page_name = $routeParams.catname;
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("");
-    }
-]);
-
-CAControllers.controller('TagAllCtrl', ['$scope', "BlogService", "OptionService",
-	function ($scope, BlogService, OptionService){
-        $scope.tags = BlogService.getAllTags();
-//        $scope.tagMax = BlogService.getTagMax();
-        $scope.BlogService = BlogService;
-
-        var page_name = "TAGS";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("tags");
-    }
-]);
-
-CAControllers.controller('TagOneCtrl', ['$scope', "BlogService", "OptionService", "$routeParams",
-	function ($scope, BlogService, OptionService, $routeParams){
-        $scope.tags = BlogService.getAllByTag($routeParams.tagname);
-        $scope.tagname = $routeParams.tagname;
-
-        $scope.years = {};
-
-        var page_name = $routeParams.tagname;
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("");
-    }
-]);
-
-CAControllers.controller('ArchiveCtrl', ['$scope', "BlogService", "OptionService",
-	function ($scope, BlogService, OptionService){
-        $scope.blogs = BlogService.getAll();
-
-        $scope.years = {};
-
-        $scope.deleteBlog = function(blog) {
-            $scope.blogs.$remove(blog);
-        }
-
-        var page_name = "ARCHIVES";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("archives");
-    }
-]);
-
-CAControllers.controller('ConfigCtrl', ['$scope', "$window", "AuthService", "OptionService", "$firebaseObject",
-    function ($scope, $window, AuthService, OptionService, $firebaseObject){
-        $scope.OptionService = OptionService;
-        var basic = OptionService.basic();
-        
-        basic.$loaded().then(function(data){
-            $scope.sitename = basic.sitename;
-            $scope.username = basic.username;
-            $scope.motto = basic.motto;
-            $scope.navbar_text = basic.navbar_text;
-            $scope.github = basic.github;
-            $scope.facebook = basic.facebook;
-            $scope.twitter = basic.twitter;
-            $scope.weibo = basic.weibo;
-            $scope.weibo_link = basic.weibo_link;
-            $scope.language = basic.language;
-        });
-
-        $scope.AuthService = AuthService;
-
-        $scope.editBasicOption = function() {
-
-            basic.sitename = $scope.sitename;
-            basic.username = $scope.username;
-            basic.motto = $scope.motto;
-            basic.navbar_text = $scope.navbar_text;
-            basic.language = $scope.language=="zh"||$scope.language=="en"?$scope.language:"en";
-            // basic.github = $scope.github;
-            basic.facebook = $scope.facebook;
-            basic.twitter = $scope.twitter;
-            basic.weibo = $scope.weibo;
-            basic.weibo_link = $scope.weibo_link;
-
-            basic.$save().then(function(ref) {
-                ref.key() === basic.$id; // true
-                alert("Website Configs Updated!");
-            }, function(error) {
-              console.log("Error:", error);
-            });
-        }
-
-        var page_name = "CONFIG";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("config");
-    }
-]);
-
-CAControllers.controller('LoginCtrl', ['$scope', "$window", "AuthService", "OptionService",
-    function ($scope, $window, AuthService, OptionService){
-        $scope.isLoggedIn = AuthService.isLoggedIn();
-        $scope.AuthService = AuthService;
-
-        if($scope.isLoggedIn){
-            $window.location.href = "#/";
-        }
-
-
-        var page_name = "LOGIN";
-        var site_name = OptionService.setSiteTitle(page_name);
-        OptionService.setCurrentNav("");
     }
 ]);
